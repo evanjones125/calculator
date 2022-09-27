706a622861fd0415caba2dc9de8fe17a74d32dd2
Evan Jones <evanjones125@gmail.com>
2022-09-27 12:25:00 -0400

    add functionality for >1 digit entries

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const divide = (x, y) => x / y;
 // let onOp = false;
 
 // create variables to store values inputted by user before and after operation button
-let a, b, currentOp, output;
+let currentOp, output;
+let a = '', b = '';
 
 const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const operations = ['+', '-', '*', '/'];
@@ -29,34 +30,43 @@ const display = document.querySelector('#display');
 // push button text content to output when clicked
 const grid = document.querySelector('#calculator');
 grid.addEventListener('click', e => {
-    // assign b the second numeric input if a is not empty
-    if (nums.includes(e.target.textContent) && a) {
-        b = Number(e.target.textContent);
+    // assign b the second numeric input if a is not empty and operation has been declared
+    if (nums.includes(e.target.textContent) && a && currentOp) {
+        b += e.target.textContent;
         output = b;
     }
     
     // assign a the first numeric input if a is empty
-    if (nums.includes(e.target.textContent) && !a) {
-        a = Number(e.target.textContent);
+    if (nums.includes(e.target.textContent) && !currentOp) {
+        a += e.target.textContent;
         output = a;
     }
 
     // if +-*/ is clicked, update currentOp
     if (operations.includes(e.target.textContent)) {
         // check to make sure a has a value
-        if (a) currentOp = e.target.textContent;
+        if (a && b === '') currentOp = e.target.textContent;
+
+        // if +-*/ is clicked and a and b have values, update output
+        if (a !== '' & b !== '') {
+            output = operate(Number(a), Number(b), currentOp);
+            currentOp = e.target.textContent;
+            a = output;
+            b = '';
+        }
     }
 
     // if = is clicked, calculate output
     if (e.target.textContent === '=') {
-        output = operate(a, b, currentOp);
+        output = operate(Number(a), Number(b), currentOp);
         a = output;
+        b = '';
     }
 
     // if AC is clicked, clear output, a, b, and currentOp
     if (e.target.textContent === 'AC') {
-        a = undefined;
-        b = undefined;
+        a = '';
+        b = '';
         currentOp = undefined;
         output = undefined;
     }
